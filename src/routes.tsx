c48171b51aa93765f436b6a8ed97d4ab0ef97c93
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-23 20:48:16 +0100

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,41 +1,44 @@
 import { Routes, Route } from "react-router-dom";
 
+
 import Home from "./components/home";
 import Materieles from "./components/materieles";
 import Droits from "./components/droits";
 import Services from "./components/services";
+import Education from "./components/education";
+import Sport from "./components/sport";
+import Travail from "./components/travail";
+import Natation from "./components/natation";
+import Mobilite from "./components/mobilite";
+import Fauteuilroulant from "./components/fauteuilroulant";
+import Fauteuilroulantelectrique from "./components/fauteuilroulantelectrique";
+import Deambulateur from "./components/deambulateur";
+import Levepersonne from "./components/levepersonne";
+import Cannedemarche from "./components/cannedemarche";
+import Rampedacces from "./components/rampedacces";
 
-import Education from "./components/Education";
-import Sport from "./components/Sport";
-import Travail from "./components/Travail";
-import Natation from "./components/Natation";
-import Mobilite  from "./components/Mobilite";
-
-
-
-
-
-function routes() {
+function RoutesApp() {
   return (
     <Routes>
-
+    
       <Route path="/" element={<Home />} />
-      <Route path="/Home" element={<Home />} />
-      <Route path="/Materieles" element={<Materieles />} />
-      <Route path="/Droits" element={<Droits />} />
-      <Route path="/Services" element={<Services />} />
-
-
-
-      <Route path="/Education" element={<Education />} />
-      <Route path="/Sport" element={<Sport />} />
-      <Route path="/Travail" element={<Travail />} />
-
-      <Route path="/Natation" element={<Natation />} />
-      <Route path="/Mobilite" element={<Mobilite />} />
-
+      <Route path="/home" element={<Home />} />
+      <Route path="/materieles" element={<Materieles />} />
+      <Route path="/droits" element={<Droits />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/education" element={<Education />} />
+      <Route path="/sport" element={<Sport />} />
+      <Route path="/travail" element={<Travail />} />
+      <Route path="/natation" element={<Natation />} />
+      <Route path="/mobilite" element={<Mobilite />} />
+      <Route path="/fauteuilroulantelectrique" element={<Fauteuilroulantelectrique />} />
+      <Route path="/fauteuilroulant" element={<Fauteuilroulant />} />
+      <Route path="/deambulateur" element={<Deambulateur />} />
+      <Route path="/levepersonne" element={<Levepersonne />} />
+      <Route path="/cannedemarche" element={<Cannedemarche />} />
+      <Route path="/rampedacces" element={<Rampedacces />} />
     </Routes>
   );
 }
 
-export default routes;
+export default RoutesApp;
