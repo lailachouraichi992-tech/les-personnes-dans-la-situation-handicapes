a485dd914a190a0c8745040e5bda27ecf2972be7
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-27 17:47:59 +0100

    Create deambulateur.tsx

---
+++ src/components/deambulateur.tsx
@@ -0,0 +1,143 @@
+import '../styles/deambulateur.css'
+
+export default function Deambulateur() {
+
+    return (
+
+        <>
+
+
+    <div className="BOX-W">
+
+
+
+     <h6  id="first-h"> Deambulateur  </h6>
+
+
+      <p id="first-p"> support avec quatre pieds ou roues pour aider à marcher en toute sécurité.</p>
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-A">
+
+
+                <img id="img-1"
+
+                    src="/img/materiels pour les hadicapes/rollator-dynamico-dinterieur-pour-enfant-6-a-8-ans.jpg" alt="home image1" />
+
+        
+
+                 <h3 className="h-41">prix : 900 DH </h3>  
+                 
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-B">
+
+
+                <img id="img-2" src="/img/materiels pour les hadicapes/grillo-g2-photo-avec-personne.jpg" alt="home image1" />
+
+        
+
+        <h3 className="h-41"> prix : 900 DH   </h3>
+
+
+                
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-C">
+
+
+             <img id="img-3" src="/img/materiels pour les hadicapes/Rollator-en-aluminium.jpg" alt="home image1" />
+
+
+               <h3 className="h-41"> prix : 900 DH </h3>
+
+
+       </div>
+
+
+
+
+            <div className="BOX-D">
+
+
+    <img id="img-4" src="/img/materiels pour les hadicapes/rollateur_avec_si_ge.jpg" alt="home image1" />
+
+        
+
+                <h3 className="h-41"> prix : 900 DH   </h3> 
+
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-E">
+
+
+                <img id="img-5" src="/img/materiels pour les hadicapes/Rollator-4-Roues-Prix-Maroc.webp" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 900 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-F">
+
+
+                <img id="img-6" src="/img/materiels pour les hadicapes/Anti-fall-Height-Adjustable-Elderly-Mobility-Training-Walker-Aid-Stand-up-Gait-Rehabilitation-Therapy-Equipment-for-Disabled.jpg_300x300.avif" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 900 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+        </>
+
+    )
+};
