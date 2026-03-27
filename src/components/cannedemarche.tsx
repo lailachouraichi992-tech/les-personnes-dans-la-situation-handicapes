b8aec97b1eb3f0c1ae53510daa1febd26ec37d22
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-27 17:43:38 +0100

    Create cannedemarche.tsx

---
+++ src/components/cannedemarche.tsx
@@ -0,0 +1,137 @@
+import '../styles/cannedemarche.css'
+
+
+
+export default function Cannedemarche() {
+
+    return (
+
+        <>
+
+
+    <div className="BOX-W">
+
+
+
+     <h6  id="first-h">  Canne-de-marche  </h6>
+
+
+      <p id="first-p"> La Canne-de-marche est installée dans les escaliers, la salle de bain ou les toilettes pour aider la personne à se tenir et éviter les chutes.</p>
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-A">
+
+
+                <img id="img-1"
+
+                    src="img/materiels pour les hadicapes/escaliers-avec-des-cannes-anglaises.jpg" alt="home image1" />
+
+        
+
+                 <h3 className="h-41">prix : 600 DH </h3>  
+                 
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-B">
+
+
+                <img id="img-2" src="/img/materiels pour les hadicapes/rambarde-exterieur.webp" alt="home image1" />
+
+        
+
+        <h3 className="h-41"> prix : 600 DH   </h3>
+
+
+                
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-C">
+
+
+             <img id="img-3" src="/img/materiels pour les hadicapes/b1e9af95ac070d96768a3b12f8f7f918.jpg" alt="home image1" />
+
+
+               <h3 className="h-41"> prix : 600 DH </h3>
+
+
+       </div>
+
+
+
+
+            <div className="BOX-D">
+
+        <img id="img-4" src="/img/materiels pour les hadicapes/Meilleure-rampe-d-escalier-en-fer-forg-designs-modernes-de-rampes-en-fer.avif" alt="home image1" />
+
+        <h3 className="h-41"> prix : 600 DH   </h3> 
+
+            </div>
+
+
+
+            <div className="BOX-E">
+
+
+                <img id="img-5" src="/img/materiels pour les hadicapes/Plan-de-travail-7-1.jpg" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 600 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-F">
+
+
+                <img id="img-6" src="/img/materiels pour les hadicapes/garde-corps-bois-exterieur.webp" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 600 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+        </>
+
+    )
+};
