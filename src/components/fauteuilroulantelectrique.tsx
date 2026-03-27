9de87fa7ffb0187d9f76bd93aae0542def2cd0e0
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-27 17:41:29 +0100

    Create fauteuilroulantelectrique.tsx

---
+++ src/components/fauteuilroulantelectrique.tsx
@@ -0,0 +1,140 @@
+import '../styles/fauteuilroulantelectrique.css'
+
+
+
+export default function Fauteuilroulantelectrique () {
+
+    return (
+
+        <>
+
+
+    <div className="BOX-W">
+
+
+
+     <h6  id="first-h"> Fauteuil-Roulant-Électrique  </h6>
+
+
+      <p id="first-p"> Le Fauteuil-Roulant-Électrique est un fauteuil conçu pour offrir un confort optimal et une mobilité facile. Son design ergonomique et ses roues adaptées permettent une utilisation confortable dans divers environnements fonctionne avec une batterie et se contrôle avec un joystick.</p>
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-A">
+
+
+                <img id="img-1"
+
+                    src="/img/materiels pour les hadicapes/Wheelchairs.png" alt="home image1" />
+
+        
+
+                 <h3 className="h-41">prix : 14 400 DH </h3>  
+                 
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-B">
+
+
+        <img id="img-2" src="/img/materiels pour les hadicapes/FAUTEUIL-ROULANT-ELECTRIQUE-IMC-ADULTE.jpg" alt="home image1" />
+
+        
+
+        <h3 className="h-41"> prix : 14 400 DH   </h3>
+
+
+                
+
+
+
+
+
+            </div>
+
+
+
+
+    <div className="BOX-C">
+        <img id="img-3" src="/img/materiels pour les hadicapes/71yxNquXG8L._AC_SL400_.jpg" alt="home image1" />
+        <h3 className="h-41"> prix : 14 400 DH </h3>
+
+    </div>
+
+
+
+
+            <div className="BOX-D">
+
+
+    <img id="img-4" src="/img/materiels pour les hadicapes/71Xv+y0OV-L._AC_SL1500_.jpg" alt="home image1" />
+
+        
+
+                <h3 className="h-41"> prix : 14 400 DH   </h3> 
+
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-E">
+
+
+                <img id="img-5" src="/img/materiels pour les hadicapes/Prix-des-fauteuils-roulants-lectriques-pour-les-personnes-g-es-au-Maroc-fauteuil-roulant-lectrique-pliable.avif" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 14 400 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-F">
+
+
+                <img id="img-6" src="/img/materiels pour les hadicapes/fauteuil-roulant-electrique-sunrise-q50-r-saq50r43.webp" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 14 400 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+        </>
+
+    )
+};
