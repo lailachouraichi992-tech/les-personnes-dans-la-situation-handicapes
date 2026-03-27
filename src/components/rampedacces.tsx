218053fb45c8e4a90115ab5c26f82b17eefe0e50
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-27 17:42:46 +0100

    Create rampedacces.tsx

---
+++ src/components/rampedacces.tsx
@@ -0,0 +1,142 @@
+import '../styles/rampedacces.css'
+
+export default function Rampedacces() {
+
+    return (
+
+        <>
+        
+    <div className="BOX-W">
+
+
+
+     <h6  id="first-h">  Rampe-Dacces  </h6>
+
+
+      <p id="first-p"> Ces rampes permettent aux personnes en fauteuil roulant de monter les escaliers ou entrer dans un bâtiment plus facilement.  </p>
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-A">
+
+
+                <img id="img-1"
+
+                    src="/img/materiels pour les hadicapes/rampe-double-pliante-ouverte-scaled-1.jpg" alt="home image1" />
+
+        
+
+                 <h3 className="h-41">prix : 2 900 DH </h3>  
+                 
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-B">
+
+
+                <img id="img-2" src="/img/materiels pour les hadicapes/26246-500.jpg" alt="home image1" />
+
+        
+
+        <h3 className="h-41"> prix : 2 900 DH   </h3>
+
+
+                
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-C">
+
+
+             <img id="img-3" src="/img/materiels pour les hadicapes\rampa-b4c-ptc-1-600x338.jpeg" alt="home image1" />
+
+
+               <h3 className="h-41"> prix : 2 900 DH </h3>
+
+
+       </div>
+
+
+
+
+            <div className="BOX-D">
+
+
+    <img id="img-4" src="/img/materiels pour les hadicapes/Rampe-dAcces-en-Aluminium-a-Pliage-Multiple-1.webp" alt="home image1" />
+
+        
+
+                <h3 className="h-41"> prix : 2 900 DH   </h3> 
+
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-E">
+
+
+                <img id="img-5" src="/img/materiels pour les hadicapes/rampe-d-acces-pliable-en-aluminium.webp" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 2 900 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-F">
+
+
+                <img id="img-6" src="/img/image des services/rampe-d-handicap-menant-à-la-piscine-6106018 (1).webp" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 2 900 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+        </>
+
+    )
+};
