1ff225505c0920d14737a23da9a14966be2b347a
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-03-27 17:44:31 +0100

    Create levepersonne.tsx

---
+++ src/components/levepersonne.tsx
@@ -0,0 +1,145 @@
+import '../styles/levepersonne.css'
+
+
+
+export default function Levepersonne() {
+
+    return (
+
+        <>
+
+
+    <div className="BOX-W">
+
+
+
+     <h6  id="first-h">  Leve-Personne  </h6>
+
+
+      <p id="first-p"> Leve-Personne est un appareil conçu pour offrir un soutien optimal et une mobilité facile. Son design ergonomique et ses roues adaptées permettent une utilisation confortable dans divers environnements.</p>
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-A">
+
+
+                <img id="img-1"
+
+                    src="/img/materiels pour les hadicapes/leve-personne.jpg" alt="home image1" />
+
+        
+
+                 <h3 className="h-41">prix : 30 000 DH </h3>  
+                 
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-B">
+
+
+                <img id="img-2" src="/img/materiels pour les hadicapes/leve-personne-nausifly-compact.jpg" alt="home image1" />
+
+        
+
+        <h3 className="h-41"> prix : 30 000 DH   </h3>
+
+
+                
+
+
+
+
+
+            </div>
+
+
+
+
+            <div className="BOX-C">
+
+
+             <img id="img-3" src="/img/materiels pour les hadicapes/3208389.jpg" alt="home image1" />
+
+
+               <h3 className="h-41"> prix : 30 000 DH </h3>
+
+
+       </div>
+
+
+
+
+            <div className="BOX-D">
+
+
+    <img id="img-4" src="/img/materiels pour les hadicapes/Leve-personne-SUNLITT-MAJOR-175.jpg" alt="home image1" />
+
+        
+
+                <h3 className="h-41"> prix : 30 000 DH   </h3> 
+
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-E">
+
+
+                <img id="img-5" src="/img/materiels pour les hadicapes\rebotec-leve-personne-electrique-mobile-avec-sangle-filet-en-u-universelle-250-re32001.jpg" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 30 000 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+            <div className="BOX-F">
+
+
+                <img id="img-6" src="/img/materiels pour les hadicapes/7001863.jpg" alt="home image1" />
+
+            
+                <h3 className="h-41"> prix : 30 000 DH  </h3> 
+                
+
+
+
+
+
+            </div>
+
+
+
+
+
+
+
+
+        </>
+
+    )
+};
