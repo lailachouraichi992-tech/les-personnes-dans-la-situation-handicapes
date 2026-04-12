3eb7ce2794e2e8cab24593eadd91f116d9cdfc77
lailachouraichi992-tech <lailachouraichi992@gmail.com>
2026-04-12 18:22:38 +0100

    Refactor Link components to use 'id' instead of 'className'

--- src/components/droits.tsx
+++ src/components/droits.tsx
@@ -12,12 +12,13 @@ export default function Droits(){
         <>    
 
 
-        
-<div className="container-main">
-<div className="container-A">
+        <div id="Container">
+          <h1> Droit-des-personnes-en-situation-d'handicapes</h1>
+<div id="container-A">
 
 
-     <Link className= "home" to="/education"> Droit-de-leducation </Link>
+
+     <Link id= "home" to="/education"> Droit-de-leducation </Link>
        
 
  <img  id= "img-AZ" src="/img/image des services/disability-kids-school-educational-projects-260nw-2036936300.webp" alt="home image1" />
@@ -27,9 +28,9 @@ export default function Droits(){
 
 
     
-<div className ="container-B">
+<div id ="container-B">
 
-   <Link className= "home" to="/sport"> Droit-au-sport </Link>
+   <Link id= "home" to="/sport"> Droit-au-sport </Link>
 
    <img  id= "img-AB"  src="/img/image des services/Lokomat.jpg" alt="home image1" />
 
@@ -44,9 +45,9 @@ export default function Droits(){
 
 
     
-<div className ="container-C">
+<div id ="container-C">
 
-      <Link className="home" to="/travail"> Droit-du-travail </Link>
+      <Link id="home" to="/travail"> Droit-du-travail </Link>
 
 
    <img  id= "img-AC" src="/img/image des services/istockphoto-1483052139-612x612.jpg" alt="home image1" />
@@ -61,9 +62,9 @@ export default function Droits(){
 
 
     
-<div className ="container-D">
+<div id ="container-D">
 
-      <Link className= "home" to="/natation"> Droit-a-la-natation </Link>
+      <Link id= "home" to="/natation"> Droit-a-la-natation </Link>
 
    <img  id= "img-AD"  src="/img/image des services/rampe-d-handicap-menant-à-la-piscine-6106018 (1).webp" alt="home image1" />
 
@@ -72,12 +73,12 @@ export default function Droits(){
 
 
     
-<div className ="container-E">
+<div id ="container-E">
 
   
-   <Link className= "home" to="/mobilite"> Droit-a-la-mobilité </Link>
+   <Link id= "home" to="/mobilite"> Droit-a-la-mobilité </Link>
 
-   <img className= "img-AE"  src="/img/materiels pour les hadicapes/leve-personne.jpg" alt="home image1" />
+   <img id= "img-AE"  src="/img/materiels pour les hadicapes/leve-personne.jpg" alt="home image1" />
 
 
 
@@ -86,9 +87,6 @@ export default function Droits(){
 
 </div>
 </div>
+
 
   </> )} 
-
-
-
-
